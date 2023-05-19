bae39460196dd9b0b1165c1933440d8849a87af8
Laura Ferrando Ferrero <lauraferrandof@gmail.com>
2023-05-19 18:46:23 +0000

    feat: implement shake functionality

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // Select the elements on the page
 const canvasEl = document.querySelector("#etch-a-sketch");
 const canvasCtx = canvasEl.getContext("2d");
+const shakeBtn = document.querySelector(".shake");
 
 // Setup our canvas for drawing
-
 // Style the canvas line
 const MOVE_AMOUNT = 10;
 canvasCtx.lineJoin = "round";
 canvasCtx.lineCap = "round";
 canvasCtx.lineWidth = MOVE_AMOUNT;
 
-// Create random x and y starting coordinates on the canvas based on the canvas width and height
-// We get a random integer number (rounded down) between 0 and the canvas width/height
 const { width, height } = canvasEl;
-let x = Math.floor(Math.random() * width);
-let y = Math.floor(Math.random() * height);
+let x;
+let y;
 
-// Put a marker on the canvas based on the random starting coordinates
-canvasCtx.beginPath();
-canvasCtx.moveTo(x, y);
-canvasCtx.lineTo(x, y);
-canvasCtx.stroke();
+// Add marker to random position
+function addMarker() {
+  // Create random x and y starting coordinates on the canvas based on the canvas width and height
+  // We get a random integer number (rounded down) between 0 and the canvas width/height
+  x = Math.floor(Math.random() * width);
+  y = Math.floor(Math.random() * height);
+
+  canvasCtx.beginPath();
+  canvasCtx.moveTo(x, y);
+  canvasCtx.lineTo(x, y);
+  canvasCtx.stroke();
+}
 
 // Draw function
 function draw({ key }) {
@@ -58,5 +63,29 @@ function handleKey(e) {
   }
 }
 
+// Clear function
+function clearCanvas() {
+  // Add shake animation
+  canvasEl.classList.add("shake");
+  // Clear canvas content
+  canvasCtx.clearRect(0, 0, width, height);
+  // Add new random starting point
+  addMarker();
+  // Remove shake class after animation ends
+  canvasEl.addEventListener(
+    "animationend",
+    function () {
+      canvasEl.classList.remove("shake");
+    },
+    // Remove event listener after the event is done
+    { once: true }
+  );
+}
+
 // Listen for arrow keys
 window.addEventListener("keydown", handleKey);
+// Listen for button clicks
+shakeBtn.addEventListener("click", clearCanvas);
+
+// Put a marker on the canvas to start drawing
+addMarker();
